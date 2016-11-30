b54571c0518a5aa34f5ef724a0385c058ab69633
akashmitra <akash.mitra@outlook.com>
2016-12-01 01:52:23 +0530

    Initial commit

---
+++ js/controller/exportController.js
@@ -0,0 +1,72 @@
+(function () {
+    'use strict';
+
+    angular.module('exportApp', [])
+        .controller('exportCtrl', exportCtrl)
+        .service('ExporTo', exporTo);
+
+
+    function exportCtrl($scope, ExporTo) {
+        $scope.title = "Export to XLSX";
+
+        $scope.exportData = function () {
+            var options = {
+                type: 'xlsx',
+                data: $scope.items,
+                filename: $scope.filename,
+                sheetid: $scope.filename,
+                headers: true,
+                columns: [
+                    {columnid: 'name', title: 'Name'},
+                    {columnid: 'email', title: 'Email'},
+                    {columnid: 'dob', title: 'Birthday'}
+                ]
+            }
+            ExporTo.xlsx(options);
+        };
+
+
+        $scope.filename = "Students";
+        $scope.items = [{
+            name: "Akash Mitra",
+            email: "Akash.Mitra@wexinc.com",
+            dob: "1988-12-17"
+        }, {
+            name: "Moulika Mukherjee",
+            email: "Moulika.Mukherjee@wexinc.com",
+            dob: "1992-10-25"
+        }, {
+            name: "Abhishek Ghosh",
+            email: "Abhishek.Ghosh@wexinc.com",
+            dob: "1992-06-07"
+        }];
+
+    }
+
+
+    /**
+     * Service for Export
+     */
+    function exporTo() {
+        this.xlsx = function (options) {
+            var style = {
+                sheetid: options.sheetid,
+                headers: options.headers,
+                columns: options.columns
+            };
+            try {
+                if (options.type == "xls") {
+                    console.warn('Export with warning');
+                    exportumxls('SELECT * INTO XLS("' + options.filename + '.xls",?) FROM ?', [style, options.data]);
+                }
+                else if (options.type == "xlsx") {
+                    exportumxls('SELECT * INTO XLSX("' + options.filename + '.xlsx",?) FROM ?', [style, options.data]);
+                }
+            } catch (error) {
+                console.error('Error in Exporting :: ' + error);
+            }
+        };
+    }
+
+
+}());
