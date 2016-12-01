c1c0de9d211fbdd39c70f699346c29e52fd50087
akashmitra <akash.mitra@outlook.com>
2016-12-01 16:27:58 +0530

    minor changes

--- js/controller/exportController.js
+++ js/controller/exportController.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
+    /*Controller for export App Sample Application*/
     angular.module('exportApp', [])
         .controller('exportCtrl', exportCtrl)
         .service('ExporTo', exporTo);
